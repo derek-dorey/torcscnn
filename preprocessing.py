25dfea8953b49dd772375582033e84a97a36af84
DerekJDorey <derek.dorey@gmail.com>
2019-01-07 15:54:05 -0500

    For analysis on the training data

---
+++ preprocessing.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+SENSOR_DATA_FILE = 'C:/Users/Derek/Source/Repos/torcs-1.3.7/runtimed/sensors.csv'
+
+sensor_data = pd.read_csv(SENSOR_DATA_FILE)
+sensor_data.head()
+
+sensor_data.steer.plot(title='Steering data distribution', fontsize=17, figsize=(10,5), color= 'r')
+plt.xlabel('frames')
+plt.ylabel('steering angle')
+plt.show()
+
+plt.figure(figsize=(10,8))
+sensor_data.steer.hist(bins=200, color='r')
+plt.xlabel('steering angle bins')
+plt.ylabel('counts')
+plt.xlim()
+plt.show()
+print("Dataset Size: ", len(sensor_data.steer))
+
+zero_steering = sensor_data[(sensor_data.steer > -0.01) & (sensor_data.steer < 0.01)].sample(frac=0.9)
+sensor_data = sensor_data.drop(zero_steering.index)
+
+plt.figure(figsize=(10,4))
+sensor_data.steer.hist(bins=100, color='r')
+plt.xlabel('steering angle bins')
+plt.ylabel('counts')
+plt.show()
+print("Current Dataset Size: ", len(sensor_data.steer))
