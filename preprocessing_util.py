7f5310ec1b2c1172e36d2a07a106e724fe2c07de
DerekJDorey <derek.dorey@gmail.com>
2019-01-12 15:01:23 -0500

    stable build with preprocessing_util

--- preprocessing_util.py
+++ preprocessing_util.py
@@ -1,9 +1,8 @@
+import properties as properties
 import pandas as pd
 import matplotlib.pyplot as plt
 
-SENSOR_DATA_FILE = 'C:/Users/Derek/Source/Repos/torcs-1.3.7/runtimed/sensors.csv'
-
-sensor_data = pd.read_csv(SENSOR_DATA_FILE)
+sensor_data = pd.read_csv(properties.SENSOR_DATA_DIRECTORY + properties.SENSOR_CSV_FILE)
 sensor_data.head()
 
 sensor_data.steer.plot(title='Steering data distribution', fontsize=17, figsize=(10,5), color= 'r')
@@ -12,14 +11,15 @@ plt.ylabel('steering angle')
 plt.show()
 
 plt.figure(figsize=(10,8))
-sensor_data.steer.hist(bins=1000, color='r')
+sensor_data.steer.hist(bins=100, color='r')
 plt.xlabel('steering angle bins')
 plt.ylabel('counts')
 plt.xlim()
 plt.show()
 print("Dataset Size: ", len(sensor_data.steer))
 
-zero_steering = sensor_data[(sensor_data.steer > - 0.00005) & (sensor_data.steer < 0.00005)].sample(frac=0.8)
+zero_steering = sensor_data[(sensor_data.steer > -properties.STEERING_BOUNDARY) &
+                            (sensor_data.steer < properties.STEERING_BOUNDARY)].sample(frac=properties.STRAIGHT_DRIVING_EXCLUSION_FACTOR)
 sensor_data = sensor_data.drop(zero_steering.index)
 
 plt.figure(figsize=(10,4))
