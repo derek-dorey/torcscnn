40a43b570783fd36c570ee129e4413adeace31d3
Derek Dorey <DerekDorey@cmail.carleton.ca>
2019-01-19 12:20:03 -0500

    Added algorithm that reduces training set according to steering angle bins

--- preprocessing_util.py
+++ preprocessing_util.py
@@ -2,9 +2,18 @@ import glob
 import properties as properties
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 SINGLE_CSV = False
+LEFT_BOUNDARY = -0.5
+RIGHT_BOUNDARY = abs(LEFT_BOUNDARY)
+BIN_WIDTH = 0.01
+SAMPLE_CEILING = 500
+COLUMN_NAMES = ['Unnamed: 0', '__doc__', '__init__', '__module__', 'accel', 'angle', 'brake', 'clutch', 'count', 'curLapTime',
+                'damage', 'distFromStart', 'distRaced', 'distToMiddle', 'focus', 'fps', 'fuel', 'gear', 'lastLapTime',
+                'posZ', 'racePos', 'rpm', 'speedX', 'speedY', 'speedZ', 'steer', 'totalDistFromStart', 'wheelVel']
 
+steer_bins = np.arange(LEFT_BOUNDARY, RIGHT_BOUNDARY, BIN_WIDTH)
 
 if SINGLE_CSV:
     sensor_data = pd.read_csv(properties.SENSOR_DATA_DIRECTORY + properties.SENSOR_CSV_FILE)
@@ -14,26 +23,62 @@ else:
     sensor_data_path = glob.glob(properties.SENSOR_DATA_DIRECTORY + '*.csv')
     sensor_data = pd.concat((pd.read_csv(f) for f in sensor_data_path))
 
-sensor_data.steer.plot(title='Steering Data Distribution', fontsize=17, figsize=(10,5), color= 'r')
+sensor_data.steer.plot(title='Steering Data Distribution', fontsize=17, figsize=(10, 5), color= 'r')
 plt.xlabel('Frames')
 plt.ylabel('Steering Angle')
 plt.show()
 
-plt.figure(figsize=(10,8))
-sensor_data.steer.hist(bins=100, color='r')
+plt.figure(figsize=(10, 8))
+sensor_data.steer.hist(bins=steer_bins, color='r')
 plt.xlabel('Steering Angle Bins')
 plt.ylabel('Number of Samples')
 plt.xlim()
 plt.show()
 print("Dataset Size: ", len(sensor_data.steer))
 
-zero_steering = sensor_data[(sensor_data.steer > -properties.STEERING_BOUNDARY) &
-                            (sensor_data.steer < properties.STEERING_BOUNDARY)].sample(frac=properties.STRAIGHT_DRIVING_EXCLUSION_FACTOR)
-sensor_data = sensor_data.drop(zero_steering.index)
+increment = 0
 
-plt.figure(figsize=(10,4))
-sensor_data.steer.hist(bins=100, color='r')
+sampled_data = pd.DataFrame(columns=COLUMN_NAMES)
+
+for sb in steer_bins:
+
+    if increment % 2 == 0:
+
+        try:
+            downsized_sample = sensor_data[
+                (sensor_data.steer >= sb) & (sensor_data.steer < (sb + BIN_WIDTH))].sample(n=500)
+
+            sampled_data = pd.concat([sampled_data, downsized_sample])
+            increment += 1
+
+        except ValueError:
+
+            downsized_sample = sensor_data[(sensor_data.steer >= sb) & (sensor_data.steer < (sb + BIN_WIDTH))]
+            sampled_data = pd.concat([sampled_data, downsized_sample])
+            increment += 1
+            continue
+
+    else:
+
+        try:
+            downsized_sample = sensor_data[
+                (sensor_data.steer > sb) & (sensor_data.steer <= (sb + BIN_WIDTH))].sample(n=500)
+
+            sampled_data = pd.concat([sampled_data, downsized_sample])
+            increment += 1
+
+        except ValueError:
+
+            downsized_sample = sensor_data[(sensor_data.steer > sb) & (sensor_data.steer <= (sb + BIN_WIDTH))]
+            sampled_data = pd.concat([sampled_data, downsized_sample])
+
+            increment += 1
+            continue
+
+
+plt.figure(figsize=(10, 4))
+sampled_data.steer.hist(bins=steer_bins, color='r')
 plt.xlabel('Steering Angle Bins')
 plt.ylabel('Number of Samples')
 plt.show()
-print("Current Dataset Size: ", len(sensor_data.steer))
+print("Current Dataset Size: ", len(sampled_data.steer))
--- properties.py
+++ properties.py
@@ -3,7 +3,7 @@ SENSOR_CSV_FILE = 'E_Track_6_Sensors.csv'
 
 # [-STEERING_BOUNDARY < x < STEERING_BOUNDARY] determines whether image with steering angle 'x'
 # is categorized as driving straight
-STEERING_BOUNDARY = 0.00100
+STEERING_BOUNDARY = 0.01
 
 # STRAIGHT_DRIVING_EXCLUSION_FACTOR determines the proportion of straight driving images to be excluded
 # (e.g. a value of 0.8 will exclude 80% of the images categorized as straight driving)
